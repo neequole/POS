b905095845ea47e2ab7bfaf174b39c7205347fde
willgrace <graycelsantos@gmail.com>
2013-05-29 14:11:29 +0800

    updated-successEdit
    by graycel

--- application/views/admin/successEdit.php
+++ application/views/admin/successEdit.php
@@ -6,22 +6,23 @@
 
 	foreach ($myitem as $r) {
 
-  echo 'Bar Code: '.$r->bar_code.'<br />
-		Item Code: '.$r->item_code.'<br>
-		Description 1 (brandm sub-brand): '.$r->desc1.'<br>
-		Description 2 (product_name): '.$r->desc2.'<br>
-		Description 3 (variant, flavor): '.$r->desc3.'<br>
-		Description 4 (size, packaging): '.$r->desc4.'<br>
-		Group: '.$r->group.'<br>
-		Classification 1: '.$r->class1.' <br>
-		Classification 2: '.$r->class2.'<br>
-		Cost: '.$r->cost.'<br>
-		Retail Price: '.$r->retail_price.'<br>
-		Model Quantity: '.$r->model_quantity.'<br>
-		Supplier Code: '.$r->supplier_code.'<br>
-		Manufacturer: '.$r->manufacturer.'<br>
-		Quantity:  '.$r->quantity.'<br>
-		Reorder Point: '.$r->reorder_point.'<br>';
+  echo '
+  		<b>Bar Code:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->bar_code.'<br />
+		<b>Item Code:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->item_code.'<br>
+		<b>Description 1 (brandm sub-brand):</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->desc1.'<br>
+		<b>Description 2 (product_name):</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->desc2.'<br>
+		<b>Description 3 (variant, flavor):</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->desc3.'<br>
+		<b>Description 4 (size, packaging):</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->desc4.'<br>
+		<b>Group:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->group.'<br>
+		<b>Classification 1:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->class1.' <br>
+		<b>Classification 2:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->class2.'<br>
+		<b>Cost:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->cost.'<br>
+		<b>Retail Price:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->retail_price.'<br>
+		<b>Model Quantity:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->model_quantity.'<br>
+		<b>Supplier Code:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->supplier_code.'<br>
+		<b>Manufacturer:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->manufacturer.'<br>
+		<b>Quantity:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->quantity.'<br>
+		<b>Reorder Point:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$r->reorder_point.'<br>';
 
 	
 		}
