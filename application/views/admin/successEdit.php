8a33015e594c4edc02b303120897b69e84ec72ab
willgrace <graycelsantos@gmail.com>
2013-05-29 13:33:07 +0800

    Success Edit Updated
    new viewing style

--- application/views/admin/successEdit.php
+++ application/views/admin/successEdit.php
@@ -2,55 +2,29 @@
 	
 		echo '<h3> ITEM UPDATED!</h3>';
 	
-			echo '<div id="view_item" class="view">';
-
-				echo '<table border="1px solid brown">
-				<tr>
-					<th> Bar Code </th>
-					<th> Item Code</th>
-					<th> Description 1 </th>
-					<th> Description 2 </th>
-					<th> Description 3 </th>
-					<th> Description 4 </th>
-					<th> Group </th>
-					<th> Classification 1 </th>
-					<th> Classification 2 </th>
-					<th> Cost </th>
-					<th> Retail Price </th>
-					<th> Model Quantity </th>
-					<th> Supplier Code </th>
-					<th> Manufacturer </th>
-					<th> Quantity </th>
-					<th> Reorder Point </th>
-					<th> Action </th>
-				</tr>';
-			
 			$myitem = $this->pos_model->get_edit_item($edit);
 
-		foreach ($myitem as $r) {
-				echo '<tr>';
-				echo '<td>'.$r->bar_code.'</td>';
-				echo '<td>'.$r->item_code.'</td>';
-				echo '<td>'.$r->desc1.'</td>';
-				echo '<td>'.$r->desc2.'</td>';
-				echo '<td>'.$r->desc3.'</td>';
-				echo '<td>'.$r->desc4.'</td>';
-				echo '<td>'.$r->group.'</td>';
-				echo '<td>'.$r->class1.'</td>';
-				echo '<td>'.$r->class2.'</td>';
-				echo '<td>'.$r->cost.'</td>';
-				echo '<td>'.$r->retail_price.'</td>';
-				echo '<td>'.$r->model_quantity.'</td>';
-				echo '<td>'.$r->supplier_code.'</td>';
-				echo '<td>'.$r->manufacturer.'</td>';
-				echo '<td>'.$r->quantity.'</td>';
-				echo '<td>'.$r->reorder_point.'</td>';
-				$edit = $r->item_code;
-				echo '<td>'.anchor('admin/delete_item/'.$edit,'Delete');
-				echo ' '.anchor('admin/goto_edit_item/'.$edit, 'Edit').'</td>';
-				echo '</tr>';
-			}
-			echo '</table></div>';
+	foreach ($myitem as $r) {
+
+  echo 'Bar Code: '.$r->bar_code.'<br />
+		Item Code: '.$r->item_code.'<br>
+		Description 1 (brandm sub-brand): '.$r->desc1.'<br>
+		Description 2 (product_name): '.$r->desc2.'<br>
+		Description 3 (variant, flavor): '.$r->desc3.'<br>
+		Description 4 (size, packaging): '.$r->desc4.'<br>
+		Group: '.$r->group.'<br>
+		Classification 1: '.$r->class1.' <br>
+		Classification 2: '.$r->class2.'<br>
+		Cost: '.$r->cost.'<br>
+		Retail Price: '.$r->retail_price.'<br>
+		Model Quantity: '.$r->model_quantity.'<br>
+		Supplier Code: '.$r->supplier_code.'<br>
+		Manufacturer: '.$r->manufacturer.'<br>
+		Quantity:  '.$r->quantity.'<br>
+		Reorder Point: '.$r->reorder_point.'<br>';
+
+	
+		}
 		
 	
 ?>
